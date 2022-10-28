b6e809d087b08bc360cfe32e6759b76902dd23be
oak072544 <oak072544@gmail.com>
2022-10-28 14:13:14 +0700

    project[complete2]

--- miniPROJECT/miniPROJECT.ino
+++ miniPROJECT/miniPROJECT.ino
@@ -1,94 +1,88 @@
 /*
-#include <LiquidCrystal.h>
-LiquidCrystal LCD(A4,A5,0,1,2,3);
+  #include <LiquidCrystal.h>
+  LiquidCrystal LCD(A4,A5,0,1,2,3);
 */
 
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C LCD(0x27, 16, 2); // LiquidCrystal_I2C lcd(0x3F, 16, 2);
-/* 
+/*
   A4  SDA
   A5  SCL
- */
+*/
 
+//#define THERMISTORPIN A0
+//#define THERMISTORVALUE 10000
+//#define TEMPERATUREVALUE 25
+//#define SAMPLESNUM 5
+//#define BCOEFFICIENT 3950
+//#define SERIESRESISTOR 10000
+//int samples[SAMPLESNUM];
+//int i;
+
+#define R0 10000.0
 #define RT0 10000   // Ω
-#define B 3950      // K 3435p 3950m
-float a = A0; //TherPIN
+#define B 3950     // K 3435p 3950m
+float a = A5; //TherPIN
 
 #define VCC 5    //Supply voltage
-#define R 10000  //R=10KΩ
+#define R 10000.0  //R=10KΩ
 
-#define PWMpin 9 
+#define PWMpin 9
 
-int MAX_temp = 70;
+int MAX_temp = 40;
+
 
 //Variables
-float RT, VR, ln, Temp, T0, Read;
+float RT, VR, ln, Temp, T0, Read,ReadV;
+float LMval,mv, cel;
+float tempPin = A0;   //LM
 
-float LMval;
-float tempPin = A2;   //LM
 
 void setup() {
   Serial.begin(9600);
-  pinMode(a,INPUT);
+//  analogReference(EXTERNAL);
   T0 = 25 + 273.15;                 //Temperature T0 from datasheet, conversion from Celsius to kelvin
-
-  
   //LCD.begin(16,2);
-
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
   LCD.begin();
   LCD.backlight();
-  
   LCD.print("Temp T : ");
 }
 
 void loop() {
-  Read = analogRead(a);              //Acquisition analog value Read
-  Read = (5.00 / 1023.00) * Read;      //Conversion to voltage
-  VR = VCC - Read;
-  RT = Read / (VR / R);               //Resistance of RT
-
-  ln = log(RT / RT0);
-  Temp = (1 / ((ln / B) + (1 / T0))); //Temperature from sensor
-
-  Temp = Temp - 273.15;                 //Conversion to Celsius
-
-
+  Read = analogRead(A1);
+  LMval = analogRead(tempPin);
+  VR = (Read*5.0)/1024.0;
+  RT = ((5.0*R)/VR)-R;
+  ln = log(RT/R0)/B;
+  Temp = (1.0/(ln+(1.0/298.15)))-273.15;
+  
   Serial.print("Temperature Ther:");
-  Serial.print("\t");
-  Serial.print(Temp);       //Conversion to c
-  Serial.print("C\t\t");
-  Serial.print(Temp + 273.15);        //Conversion to Kelvin
-  Serial.print("K\t\t");
-  Serial.print((Temp * 1.8) + 32);    //Conversion to Fahrenheit
-  Serial.println("F");
-
-  LCD.setCursor(10,0);
+  Serial.println(Temp);
+  LCD.setCursor(10, 0);
   LCD.print(Temp);
-
-  LMval = analogRead(A2);
-  float mv = ( LMval / 1024.0) * 5000;
-  float cel = mv / 10;
-
+  delay(1000);
+  
   Serial.print("TEMP lm = ");
-  Serial.print(cel);
-  Serial.print("*C");
-  Serial.println();
-  LCD.setCursor(0,1);
+  cel = analogRead(tempPin)*0.488;
+  Serial.println(cel);
+  LCD.setCursor(0, 1);
   LCD.print("Temp L : ");
   LCD.print(cel);
+  delay(1000);
 
-  if(Temp >= MAX_temp || cel >= MAX_temp)
+  
+  
+  if (Temp >= MAX_temp || cel >= MAX_temp)
   {
-    analogWrite(PWMpin,0);
+    analogWrite(PWMpin, 0);
   }
   else
   {
-    analogWrite(PWMpin,255); // analogWrite values from 0 to 255
+    analogWrite(PWMpin, 0); // analogWrite values from 0 to 255
   }
-  delay(1000);
-
-  //.....
-
+  
 }
