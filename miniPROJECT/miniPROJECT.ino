a956ea5883406cf2e030bbfb1169ea0e2e0e1885
oak072544 <oak072544@gmail.com>
2023-07-26 18:34:22 +0700

    last commit

--- miniPROJECT/miniPROJECT.ino
+++ miniPROJECT/miniPROJECT.ino
@@ -1,8 +1,3 @@
-/*
-  #include <LiquidCrystal.h>
-  LiquidCrystal LCD(A4,A5,0,1,2,3);
-*/
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
@@ -12,15 +7,6 @@ LiquidCrystal_I2C LCD(0x27, 16, 2); // LiquidCrystal_I2C lcd(0x3F, 16, 2);
   A5  SCL
 */
 
-//#define THERMISTORPIN A0
-//#define THERMISTORVALUE 10000
-//#define TEMPERATUREVALUE 25
-//#define SAMPLESNUM 5
-//#define BCOEFFICIENT 3950
-//#define SERIESRESISTOR 10000
-//int samples[SAMPLESNUM];
-//int i;
-
 #define R0 10000.0
 #define RT0 10000   // Ω
 #define B 3950     // K 3435p 3950m
@@ -28,11 +14,9 @@ float a = A5; //TherPIN
 
 #define VCC 5    //Supply voltage
 #define R 10000.0  //R=10KΩ
-
 #define PWMpin 9
 
-int MAX_temp = 40;
-
+int MAX_temp = 36;
 
 //Variables
 float RT, VR, ln, Temp, T0, Read,ReadV;
@@ -40,11 +24,10 @@ float LMval,mv, cel;
 float tempPin = A0;   //LM
 
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
-//  analogReference(EXTERNAL);
   T0 = 25 + 273.15;                 //Temperature T0 from datasheet, conversion from Celsius to kelvin
-  //LCD.begin(16,2);
   pinMode(A0,INPUT);
   pinMode(A1,INPUT);
   LCD.begin();
@@ -52,7 +35,8 @@ void setup() {
   LCD.print("Temp T : ");
 }
 
-void loop() {
+void loop() 
+{
   Read = analogRead(A1);
   LMval = analogRead(tempPin);
   VR = (Read*5.0)/1024.0;
@@ -82,7 +66,7 @@ void loop() {
   }
   else
   {
-    analogWrite(PWMpin, 0); // analogWrite values from 0 to 255
+    analogWrite(PWMpin, 125); // analogWrite values from 0 to 255
   }
   
 }
